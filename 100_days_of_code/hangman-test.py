6a1cba3ffbe53ad3d5ce070256ac56c63cc27dda
i-tejocote <112791861+i-tejocote@users.noreply.github.com>
2023-01-28 12:46:47 -0800

    updated: updated with instructers code

--- 100_days_of_code/hangman-test.py
+++ 100_days_of_code/hangman-test.py
@@ -15,17 +15,15 @@ display = []
 
 for letter in chosen_word:
     display += ("_")
-print(display)
 
 #TODO-2: - Loop through each position in the chosen_word;
 #If the letter at that position matches 'guess' then reveal that letter in the display at that position.
 #e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
 
-for letter in chosen_word:
-    if letter == guess:
-        print("Right")
-    else:
-        print("Wrong")
-
+for letter in range(len(chosen_word)):
+    if chosen_word[letter] == guess:
+        display[letter] = guess
+        
+print(display)
 #TODO-3: - Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
 #Hint - Don't worry about getting the user to guess the next letter. We'll tackle that in step 3.
